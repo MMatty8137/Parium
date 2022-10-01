129bde78d736963450d6500884b3c2fbc819aa71
matyasmatta <matyasmatta@email.cz>
2022-10-01 16:10:11 +0200

    Release clean test

--- main.py
+++ main.py
@@ -101,8 +101,8 @@ def main(method):
                 driver.find_element("xpath", '//*[@id="cms-app"]/div[2]/div/div/div/div[3]/button[1]').click()
                 driver.implicitly_wait(10)
                 ## xpath element location
-                stateXPath = '//*[@id="component-43773"]/div/div[1]/div[2]/div/div[6]/div[1]/div[1]/div/div/span'
-                priceXPath = '//*[@id="component-43773"]/div/div[1]/div[2]/div/div[6]/div[1]/div[2]/div'
+                stateXPath = '//*[@id="component-46557"]/div/div[1]/div[2]/div/div[6]/div[1]/div[1]/div/div/span'
+                priceXPath = '//*[@id="component-46557"]/div/div[1]/div[2]/div/div[6]/div[1]/div[2]/div'
                 ## gets availibilty and price data
                 stateOfItem = driver.find_element("xpath", stateXPath).text
                 priceOfItem = driver.find_element("xpath", priceXPath).text
--- old/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-import bs4
-
-headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-URL = "https://www.mp.cz/tablet-apple-ipad-air-2020-64gb-wi-fi-rose-gold-a-pouzity-p-115480"
-page = requests.get(URL, headers=headers)
-
-print(page.text)
